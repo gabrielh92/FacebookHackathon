e1eb2c608a333b6a446ca12c718d234f22cb69ee
gabrielh92 <gabrielh92@gmail.com>
2013-11-09 18:21:37 -0600

    Hiii

--- MainActivity.java
+++ MainActivity.java
@@ -23,10 +23,6 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-
-        boolean test = false;
-        if(test == true)
-            return;
     }
 
 
